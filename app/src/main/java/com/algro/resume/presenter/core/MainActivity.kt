e6361dc212459a4f9004767783e080e4c694638e
thegarlynch <yenaskithegar@gmail.com>
2020-07-02 11:21:20 +0700

    Move MainActivity.kt to presenter

--- app/src/main/java/com/algro/resume/presenter/core/MainActivity.kt
+++ app/src/main/java/com/algro/resume/presenter/core/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.algro.resume
+package com.algro.resume.presenter.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.algro.resume.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.main) {
 
     lateinit var navController: NavController
 
