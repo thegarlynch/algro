bde48df0626e43a82f8176ee240c3b77301830c0
thegarlynch <yenaskithegar@gmail.com>
2020-07-02 12:27:08 +0700

    Add FrontFragment.kt Layout

--- app/src/main/java/com/algro/resume/view/front/FrontFragment.kt
+++ app/src/main/java/com/algro/resume/view/front/FrontFragment.kt
@@ -1,8 +1,15 @@
 package com.algro.resume.view.front
 
+import android.os.Bundle
+import android.view.View
 import com.algro.resume.helper.fragment.AbstractFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FrontFragment : AbstractFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
