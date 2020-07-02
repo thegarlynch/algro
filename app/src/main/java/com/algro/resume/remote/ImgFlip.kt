fafdf7a998fe7aac577adeb098ffd07551ac4fa8
thegarlynch <yenaskithegar@gmail.com>
2020-07-02 12:12:02 +0700

    Improve ImgFlip.kt caption

--- app/src/main/java/com/algro/resume/remote/ImgFlip.kt
+++ app/src/main/java/com/algro/resume/remote/ImgFlip.kt
@@ -12,12 +12,51 @@ interface ImgFlip {
     suspend fun getMemes() : Envelope<MemeResponse>
 
     @POST("caption_image")
-    suspend fun captionImage() : Envelope<Nothing?>
+    suspend fun captionImage(request : CaptionRequest) : Envelope<CaptionResponse>
 
 }
 
 @JsonClass(generateAdapter = true)
 data class MemeResponse(val memes : List<Meme>)
+
+@JsonClass(generateAdapter = true)
+data class CaptionRequest(
+    @Json(name = "template_id") val templateId : String,
+    val username : String,
+    val password : String,
+    /**
+     *  Don't use [topText] and [bottomText]
+     *  if [boxes] are used
+     */
+    @Json(name = "text0") val topText : String?,
+    @Json(name = "text1") val bottomText : String?,
+    /**
+     *  Either impact or arial
+     */
+    val font : String? = null,
+    /**
+     *  ex : 50px
+     */
+    @Json(name = "max_font_size") val maxFontSize : String?,
+    val boxes : List<Box>? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class Box(
+    val text : String,
+    val x : Int,
+    val y : Int,
+    val width : Int,
+    val height : Int,
+    /**
+     *  ex : "#ffffff"
+     */
+    val color : String,
+    /**
+     *  ex : "#000000"
+     */
+    @Json(name = "outline_color") val outlineColor : String
+)
 
 @JsonClass(generateAdapter = true)
 data class CaptionResponse(
