7418950f97f32504e403bd19840782f4a8d28878
thegarlynch <yenaskithegar@gmail.com>
2020-07-02 11:25:16 +0700

    Add NavGraph

---
+++ app/src/main/java/com/algro/resume/presenter/front/FrontFragment.kt
@@ -0,0 +1,8 @@
+package com.algro.resume.presenter.front
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FrontFragment : Fragment() {
+}
